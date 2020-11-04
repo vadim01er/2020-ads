fa6ed21ea455a7a402aea41eabacf331430c9d93
Dmitry Schitinin <dmitry.schitinin@corp.mail.ru>
2020-11-04 11:54:44 +0300

    Branch for AVL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 // Apply the java plugin to add support for Java
 plugins {
     java
-    application
 }
 
 java {
@@ -14,9 +13,6 @@ repositories {
 }
 
 dependencies {
-    // Guava primitives
-    compile("com.google.guava:guava:27.0.1-jre")
-
     // Annotation
     implementation ("org.jetbrains:annotations:16.0.3")
 
@@ -25,18 +21,9 @@ dependencies {
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.0")
 }
 
-val run by tasks.getting(JavaExec::class) {
-    standardInput = System.`in`
-}
-
 tasks {
     test {
         maxHeapSize = "128m"
         useJUnitPlatform()
     }
-}
-
-application {
-    // Define the main class for the application
-    mainClassName = "ru.mail.polis.ads.SolveTemplate"
 }
