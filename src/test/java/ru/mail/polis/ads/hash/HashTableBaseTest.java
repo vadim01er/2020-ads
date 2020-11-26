8f6a72da6204e71b7a2ad66a7f6b62a11f3d2ff7
Vadim Ershov <vadim.ershov.01@mail.ru>
2020-11-27 00:49:38 +0500

    checking new tests

--- src/test/java/ru/mail/polis/ads/hash/HashTableBaseTest.java
+++ src/test/java/ru/mail/polis/ads/hash/HashTableBaseTest.java
@@ -46,7 +46,7 @@ class HashTableBaseTest {
     
     HashTable<Key, String> newStrangeKeyTable() {
         // Use implementation
-        return null;
+        return new Table<>();
     }
 
     @Test
